77175eaa6f8e18497ab4bf366b8ac3f7292017da
Andrea Bettini <abettini99@gmail.com>
2022-10-18 16:27:30 +0200

    Added utils.py

---
+++ lib/utils.py
@@ -0,0 +1,25 @@
+import matplotlib
+import matplotlib.pyplot as plt
+
+def prepare_graphs() -> None:
+    ## Define text sizes for **SAVED** pictures (texpsize -- text export size)
+    texpsize    = [16,22,24]
+
+    ## Graphing Parameters
+    SMALL_SIZE  = texpsize[0]
+    MEDIUM_SIZE = texpsize[1]
+    BIGGER_SIZE = texpsize[2]
+
+    plt.style.use('grayscale')
+    plt.rc('font', size=MEDIUM_SIZE, family='serif')    ## controls default text sizes
+    plt.rc('axes', titlesize=MEDIUM_SIZE)                ## fontsize of the axes title
+    plt.rc('axes', labelsize=SMALL_SIZE)                ## fontsize of the x and y labels
+    plt.rc('xtick', labelsize=SMALL_SIZE)               ## fontsize of the tick labels
+    plt.rc('ytick', labelsize=SMALL_SIZE)               ## fontsize of the tick labels
+    plt.rc('legend', fontsize=SMALL_SIZE)               ## legend fontsize
+    plt.rc('figure', titlesize=BIGGER_SIZE)             ## fontsize of the figure title
+    plt.rc('text', usetex=False)
+    matplotlib.rcParams['lines.linewidth']  = 1.5
+    matplotlib.rcParams['figure.facecolor'] = 'white'
+    matplotlib.rcParams['axes.facecolor']   = 'white'
+    matplotlib.rcParams["legend.fancybox"]  = False
